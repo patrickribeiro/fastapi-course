d21048beb559c1098d62193807f11e284a9335a1
Patrick Ribeiro <patrickalribeiro@gmail.com>
2024-09-22 10:32:38 -0300

    Commit da aula sobre desenvolvimento web. Aula 02.

--- fast_zero/fast_zero/app.py
+++ fast_zero/fast_zero/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from starlette.responses import HTMLResponse
 
 app = FastAPI()
 
@@ -6,3 +7,20 @@ app = FastAPI()
 @app.get('/')
 def read_root():
     return {'message': 'Olá, mundo!'}
+
+
+@app.get('/ola-mundo', response_class=HTMLResponse)
+def ola_mundo():
+    return """
+    <html>
+        <head>
+            <title>
+                My hello world
+            </title>
+        </head>
+        <body>
+            <h1>
+                Helo, World, from Patrick!
+            </h1>
+        </body>
+    </html>"""
--- fast_zero/tests/test_app.py
+++ fast_zero/tests/test_app.py
@@ -10,3 +10,25 @@ def test_read_root_deve_retornar_ok_e_ola_mundo():
     response = client.get('/')
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá, mundo!'}
+
+
+def test_read_ola_mundo_deve_retornar_ok_e_html():
+    client = TestClient(app)
+    response = client.get('/ola-mundo')
+    assert response.status_code == HTTPStatus.OK
+    assert (
+        response.text
+        == """
+    <html>
+        <head>
+            <title>
+                My hello world
+            </title>
+        </head>
+        <body>
+            <h1>
+                Helo, World, from Patrick!
+            </h1>
+        </body>
+    </html>"""
+    )
