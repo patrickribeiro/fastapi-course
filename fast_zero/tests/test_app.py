da8c9299eeeb509a165a568651b883222cd7c2c0
Patrick Ribeiro <patrickalribeiro@gmail.com>
2024-09-22 17:01:24 -0300

    Commit do CRUD desenvolvido na aula 3. Aula 03.

--- fast_zero/tests/test_app.py
+++ fast_zero/tests/test_app.py
@@ -1,19 +1,13 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from fast_zero.app import app
-
-
-def test_read_root_deve_retornar_ok_e_ola_mundo():
-    client = TestClient(app)
+def test_read_root_deve_retornar_ok_e_ola_mundo(client):
     response = client.get('/')
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá, mundo!'}
 
 
-def test_read_ola_mundo_deve_retornar_ok_e_html():
-    client = TestClient(app)
+def test_read_ola_mundo_deve_retornar_ok_e_html(client):
     response = client.get('/ola-mundo')
     assert response.status_code == HTTPStatus.OK
     assert (
@@ -32,3 +26,93 @@ def test_read_ola_mundo_deve_retornar_ok_e_html():
         </body>
     </html>"""
     )
+
+
+def test_create_user_deve_retornar_created_nome_e_email(client):
+    response = client.post(
+        '/users',
+        json={
+            'id': 0,
+            'name': 'string',
+            'email': 'user@example.com',
+            'password': 'string',
+        },
+    )
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'name': 'string',
+        'email': 'user@example.com',
+        'id': 1,
+    }
+
+
+def test_read_user_deve_retornar_ok_e_lista_de_users(client):
+    response = client.get('/users')
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'users': [
+            {
+                'name': 'string',
+                'email': 'user@example.com',
+                'id': 1,
+            }
+        ]
+    }
+
+
+def test_update_user_deve_retornar_ok_nome_email_e_id(client):
+    response = client.put(
+        '/users/1',
+        json={
+            'name': 'patrick',
+            'email': 'patrick@ribeiro.com',
+            'password': 'novasenha',
+        },
+    )
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'name': 'patrick',
+        'email': 'patrick@ribeiro.com',
+        'id': 1,
+    }
+
+
+def test_update_user_deve_retornar_not_found_e_mensagem_not_found(client):
+    response = client.put(
+        '/users/0',
+        json={
+            'name': 'patrick',
+            'email': 'patrick@ribeiro.com',
+            'password': 'novasenha',
+        },
+    )
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found.'}
+
+
+def test_read_user_by_id_deve_retornar_ok_nome_email_e_id(client):
+    response = client.get('/users/1')
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'name': 'patrick',
+        'email': 'patrick@ribeiro.com',
+        'id': 1,
+    }
+
+
+def test_read_user_by_id_deve_retornar_not_found_e_detail_not_found(client):
+    response = client.get('/users/0')
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found.'}
+
+
+def test_delete_user_deve_retornar_ok_e_mensagem_user_deleted(client):
+    response = client.delete('/users/1')
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'User deleted.'}
+
+
+def test_delete_user_deve_retornar_not_found_e_detail_user_not_found(client):
+    response = client.delete('/users/0')
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found.'}
